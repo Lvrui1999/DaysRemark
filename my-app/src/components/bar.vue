6e1241f68dcd72131df40f4ee2ea7690e9860359
Lvrui1999 <lvrui1999@gmail.com>
2020-11-30 17:35:47 +0800

    ok

---
+++ my-app/src/components/bar.vue
@@ -0,0 +1,30 @@
+<template>
+  <div id="bar">
+    <v-container>
+        <v-banner elevation="5" width="500">
+            <v-btn elevation="2" @click="ss" >
+                <p>+1</p>     
+            </v-btn>
+            <v-content> {{val}} </v-content>
+        </v-banner>
+    </v-container>
+  </div>
+</template>
+ 
+<script>
+export default {
+  data () {
+    return {
+        val:1
+    }
+  },
+  methods :{
+    ss : function(){
+        this.val += 1
+    }
+  }
+}
+</script>
+ 
+<style>
+</style>
